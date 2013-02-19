626045b7a0d512b877e2c5b5d46d2e5a122b2884
Leyu Sisay <leyu_s@hotmail.com>
2013-02-19 15:50:12 +0300

    Renamed FT to AgeDifferenceType
    Changed the enum FT to class AgeDifferenceType with a strategy
    implementation

---
+++ Refactoring/CS/Algorithm/Algorithm/AgeDifferenceType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algorithm
+{
+    public class AgeDifferenceType
+    {
+        private readonly Func<PairAgeDifference, PairAgeDifference, PairAgeDifference> _strategy;
+        public static readonly AgeDifferenceType Closer = new AgeDifferenceType((p1, p2) => p1.AgeDifference < p2.AgeDifference ? p1 : p2);
+        public static readonly AgeDifferenceType Further = new AgeDifferenceType((p1, p2) => p1.AgeDifference > p2.AgeDifference ? p1 : p2);
+
+        public Func<PairAgeDifference, PairAgeDifference, PairAgeDifference> Strategy
+        {
+            get { return _strategy; }
+        }
+
+        private AgeDifferenceType(Func<PairAgeDifference, PairAgeDifference, PairAgeDifference> strategy)
+        {
+            _strategy = strategy;
+        }
+    }
+}
