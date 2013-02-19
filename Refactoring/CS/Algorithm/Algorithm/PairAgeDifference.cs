a1abcadeecc07efdd3119ec40746bc9b1a8ebf58
Leyu Sisay <leyu_s@hotmail.com>
2013-02-19 15:48:54 +0300

    Renamed F class to a descriptive one

---
+++ Refactoring/CS/Algorithm/Algorithm/PairAgeDifference.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Algorithm
+{
+    public class PairAgeDifference
+    {
+        public Person YongerPerson { get; set; }
+        public Person OlderPerson { get; set; }
+        public TimeSpan AgeDifference { get; set; }
+    }
+}
